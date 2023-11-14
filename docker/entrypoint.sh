8e7f9bdf49ba107204485e9a41a81d66f903f317
wangwei <wangwei@pjlab.org.cn>
2023-11-14 21:51:54 +0800

    fix bug

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [[ "$1" = "serve" ]]; then
     shift 1
     source activate InterpAny
-    cd /InterpAny-Clearer/webapp/backend && nohup flask run -p 5001 --host "0.0.0.0" &
+    cd /InterpAny-Clearer/webapp/backend && nohup python app.py &
     cd /InterpAny-Clearer/webapp/webapp && yarn && nohup yarn start &
     echo "Webapp is running on http://localhost:8080"
 else
